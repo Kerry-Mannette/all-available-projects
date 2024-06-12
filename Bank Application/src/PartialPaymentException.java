26def3625ea5a7153a48e6e792e45305377e21a4
Kerry-Mannette <kerrymannette2@gmail.com>
2024-06-12 19:45:06 -0400

    Delete Bank Application directory

--- Bank Application/src/PartialPaymentException.java
+++
@@ -1,9 +0,0 @@
-public class PartialPaymentException extends Exception{
-
-    /**
-     * <h1>Partial Payment Exception Method</h1>
-     */
-
-    public PartialPaymentException (){super("Error! Insufficient Amount.");}
-
-}

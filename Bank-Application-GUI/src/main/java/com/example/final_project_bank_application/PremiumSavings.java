ed1533f7fada775e0b68040c407a3f7fc4e92345
Kerry-Mannette <kerrymannette2@gmail.com>
2024-06-12 20:06:13 -0400

    Add files via upload

---
+++ Bank-Application-GUI/src/main/java/com/example/final_project_bank_application/PremiumSavings.java
@@ -0,0 +1,52 @@
+package com.example.final_project_bank_application;
+
+public final class PremiumSavings extends SavingsAccount {
+
+    private final double int_rate;
+
+    /**
+     * <h1>Premium Saving Method</h1>
+     * @param bal the balance argument
+     * @param own the owner argument
+     * @param ir the interest rate argument
+     */
+
+    public PremiumSavings(double bal, IndividualCustomer own, double ir) {
+        super(bal, own);
+        int_rate = ir;
+    }
+
+    /**
+     * <h1>Get Interest Rate Method</h1>
+     * @return interest rate the premium saving interest rate
+     */
+
+    public double getInt_rate() {
+        return int_rate;
+    }
+
+    /**
+     * <h1>Pay Interest Method</h1>
+     * @param amt_entered the amount argument
+     * @throws PartialPaymentException
+     */
+
+    public void payInterest(double amt_entered) throws PartialPaymentException{
+        double amt_to_pay = 0;
+        int T = 12;
+
+                amt_to_pay = (amt_entered * int_rate *T) / 100;
+
+    }
+
+    /**
+     * <h1>Premium Saving To String Method</h1>
+     * @return String.format the premium saving information
+     */
+
+    @Override
+    public String toString() {
+        return String.format("\n\n%sBalance: %.2f\nInterest Rate: %s\n ",getOwner(),getBalance(),getInt_rate());
+    }
+
+}

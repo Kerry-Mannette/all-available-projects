dfd91796ec059d94ece6ae75c0dc84cb2f2a2947
Kerry-Mannette <kerrymannette2@gmail.com>
2024-06-12 20:08:52 -0400

    Delete Bank-Application-GUI directory

--- Bank-Application-GUI/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.final_project_bank_application {
-    requires javafx.controls;
-    requires javafx.fxml;
-            
-                        
-    opens com.example.final_project_bank_application to javafx.fxml;
-    exports com.example.final_project_bank_application;
-}

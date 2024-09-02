2476476a99f5bf544f7d84ebd39599f578b31202
Kerry-Mannette <kerrymannette2@gmail.com>
2024-09-02 01:46:57 -0400

    Delete Ministry-of-Works-and-Transport directory

--- Ministry-of-Works-and-Transport/js/client_main_dashboard_menu.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "275px";/*For more information on side navigation: https://www.w3schools.com/howto/howto_js_sidenav.asp */
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-
-
